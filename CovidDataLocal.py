0f6169890d9a66f7392ad75257b88fe5ec026128
justawanderingcloud <jakub.stastka@gmail.com>
2020-10-14 22:51:15 +0200

    initial commit

---
+++ CovidDataLocal.py
@@ -0,0 +1,61 @@
+import json
+import random
+import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, "czech")
+
+article_part_1 = ["Koronavirová situace je taková, že ke dni {}",
+                  "Ke dni {}"]
+article_part_2 = ["bylo provedeno {} testů.",
+                  "bylo otestováno {} lidí na koronavirus."]
+article_part_3 = ["Aktuálně je aktivních {} případů.",
+                  "V současné době je {} aktivních případů nemoci."]
+article_part_4 = ["Zatím se vyléčilo {} nemocných.",
+                  "Aktuálně je {} nemocných."]
+article_part_5 = ["Do dnešního dne na koronavirus zemřelo {} nemocných.",
+                  "Koronavirová epidemie si do dnešního dne vyžádala {} obětí."]
+article_part_6 = ["Aktuálně je kvůli covid-19 hospitalizováno {} pacientů.",
+                  "Hospitalizovaných kvůli koronaviru je aktuálně {} pacientů."]
+article_part_7 = ["Včera bylo provedeno {} testů.",
+                  "Zdravotníci včera provedli {} testů."]
+article_part_8 = ["Během včerejšího dne se potvrdilo {} nových případů nákazy.",
+                  "Odborníci zaregistrovali {} nových potvrzených případů nemoci."]
+article_part_9 = ["Dnes se nakazilo {} lidí koronavirovou infekcí.",
+                  "Během dnešního dne je {} nových nakažených nemocí covid-19"]
+
+article_part_1_selected = random.sample(article_part_1, 1)[0]
+article_part_2_selected = random.sample(article_part_2, 1)[0]
+article_part_3_selected = random.sample(article_part_3, 1)[0]
+article_part_4_selected = random.sample(article_part_4, 1)[0]
+article_part_5_selected = random.sample(article_part_5, 1)[0]
+article_part_6_selected = random.sample(article_part_6, 1)[0]
+article_part_7_selected = random.sample(article_part_7, 1)[0]
+article_part_8_selected = random.sample(article_part_8, 1)[0]
+article_part_9_selected = random.sample(article_part_9, 1)[0]
+
+with open("zakladni-prehled.json", "r") as dataset:
+    covid_data = json.loads(dataset.read())
+
+    covid_data_processed = covid_data["data"][0]
+    data_aquisition_date = covid_data_processed.get("datum")
+    datetime_aquistition = datetime.datetime.strptime(data_aquisition_date, "%Y-%m-%d").date()
+    datetime_stamp = datetime_aquistition.strftime("%d. %b %Y")
+    tests_done_total = covid_data_processed.get("provedene_testy_celkem")
+    active_cases = covid_data_processed.get("aktivni_pripady")
+    cured_total = covid_data_processed.get("vyleceni")
+    deaths_total = covid_data_processed.get("umrti")
+    hospitalized = covid_data_processed.get("aktualne_hospitalizovani")
+    tests_done_yesterday = covid_data_processed.get("provedene_testy_vcerejsi_den")
+    confirmed_cases_yesterday = covid_data_processed.get("potvrzene_pripady_vcerejsi_den")
+    confirmed_cases_today = covid_data_processed.get("potvrzene_pripady_dnesni_den")
+
+    print(article_part_1_selected.format(datetime_stamp),
+          article_part_2_selected.format(tests_done_total),
+          article_part_3_selected.format(active_cases),
+          article_part_4_selected.format(cured_total),
+          article_part_5_selected.format(deaths_total),
+          article_part_6_selected.format(hospitalized),
+          article_part_7_selected.format(tests_done_yesterday),
+          article_part_8_selected.format(confirmed_cases_today),
+          article_part_9_selected.format(confirmed_cases_today))
---
+++ CovidRegionDataLocal.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+data = pd.read_csv("kraj-okres-nakazeni-vyleceni-umrti.csv")
+
+last_date = data[data["datum"] == "2020-10-06"]
+str_cz = last_date[last_date["kraj_nuts_kod"] == "CZ020"]
+
+str_cz_processed = str_cz.replace(["CZ020", "CZ0201", "CZ0202", "CZ0203", "CZ0204", "CZ0205", "CZ0206", "CZ0207", "CZ0208", "CZ0209", "CZ020A", "CZ020B", "CZ020C" ], ["Stredocesky kraj", "Okres Benešov", "Okres Beroun", "Okres Kladno", "Okres Kolín", "Okres Kutná Hora", "Okres Mělník", "Okres Mladá Boleslav", "Okres Nymburk", "Okres Praha-východ", "Okres Praha-západ", "Okres Příbram", "Okres Rakovník"])
+
+str_cz_processed.to_csv(r"output.csv", index=False)
