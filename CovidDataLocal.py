900a727ddf96afa8c98b0ba6ce4acdbdbd22d835
justawanderingcloud <jakub.stastka@gmail.com>
2020-10-15 09:53:56 +0200

    implemented print out into file

--- CovidDataLocal.py
+++ CovidDataLocal.py
@@ -7,24 +7,24 @@ import locale
 locale.setlocale(locale.LC_TIME, "czech")
 
 # parts for the final article
-article_part_1 = ["Koronavirová situace je taková, že ke dni {}",
-                  "Ke dni {}"]
-article_part_2 = ["bylo provedeno {} testů.",
-                  "bylo otestováno {} lidí na koronavirus."]
-article_part_3 = ["Aktuálně je aktivních {} případů.",
-                  "V současné době je {} aktivních případů nemoci."]
-article_part_4 = ["Zatím se vyléčilo {} nemocných.",
-                  "Aktuálně je {} nemocných."]
-article_part_5 = ["Do dnešního dne na koronavirus zemřelo {} nemocných.",
-                  "Koronavirová epidemie si do dnešního dne vyžádala {} obětí."]
-article_part_6 = ["Aktuálně je kvůli covid-19 hospitalizováno {} pacientů.",
-                  "Hospitalizovaných kvůli koronaviru je aktuálně {} pacientů."]
-article_part_7 = ["Včera bylo provedeno {} testů.",
-                  "Zdravotníci včera provedli {} testů."]
-article_part_8 = ["Během včerejšího dne se potvrdilo {} nových případů nákazy.",
-                  "Odborníci zaregistrovali {} nových potvrzených případů nemoci."]
-article_part_9 = ["Dnes se nakazilo {} lidí koronavirovou infekcí.",
-                  "Během dnešního dne je {} nových nakažených nemocí covid-19"]
+article_part_1 = ["Koronavirová situace je taková, že ke dni {} ",
+                  "Ke dni {} "]
+article_part_2 = ["bylo provedeno {} testů. ",
+                  "bylo otestováno {} lidí na koronavirus. "]
+article_part_3 = ["Aktuálně je aktivních {} případů. ",
+                  "V současné době je {} aktivních případů nemoci. "]
+article_part_4 = ["Zatím se vyléčilo {} nemocných. ",
+                  "Aktuálně je {} nemocných. "]
+article_part_5 = ["Do dnešního dne na koronavirus zemřelo {} nemocných. ",
+                  "Koronavirová epidemie si do dnešního dne vyžádala {} obětí. "]
+article_part_6 = ["Aktuálně je kvůli covid-19 hospitalizováno {} pacientů. ",
+                  "Hospitalizovaných kvůli koronaviru je aktuálně {} pacientů. "]
+article_part_7 = ["Včera bylo provedeno {} testů. ",
+                  "Zdravotníci včera provedli {} testů. "]
+article_part_8 = ["Během včerejšího dne se potvrdilo {} nových případů nákazy. ",
+                  "Odborníci zaregistrovali {} nových potvrzených případů nemoci. "]
+article_part_9 = ["Dnes se nakazilo {} lidí koronavirovou infekcí. ",
+                  "Během dnešního dne je {} nových nakažených nemocí covid-19. "]
 
 article_part_1_selected = random.sample(article_part_1, 1)[0]
 article_part_2_selected = random.sample(article_part_2, 1)[0]
@@ -53,13 +53,18 @@ with open("zakladni-prehled.json", "r") as dataset:
     confirmed_cases_yesterday = covid_data_processed.get("potvrzene_pripady_vcerejsi_den")
     confirmed_cases_today = covid_data_processed.get("potvrzene_pripady_dnesni_den")
 
-    # download file, process and print
-    print(article_part_1_selected.format(datetime_stamp),
-          article_part_2_selected.format(tests_done_total),
-          article_part_3_selected.format(active_cases),
-          article_part_4_selected.format(cured_total),
-          article_part_5_selected.format(deaths_total),
-          article_part_6_selected.format(hospitalized),
-          article_part_7_selected.format(tests_done_yesterday),
-          article_part_8_selected.format(confirmed_cases_today),
-          article_part_9_selected.format(confirmed_cases_today))
+    covid_article_parts = [article_part_1_selected.format(datetime_stamp),
+                           article_part_2_selected.format(tests_done_total),
+                           article_part_3_selected.format(active_cases),
+                           article_part_4_selected.format(cured_total),
+                           article_part_5_selected.format(deaths_total),
+                           article_part_6_selected.format(hospitalized),
+                           article_part_7_selected.format(tests_done_yesterday),
+                           article_part_8_selected.format(confirmed_cases_today),
+                           article_part_9_selected.format(confirmed_cases_today)]
+
+    covid_article = "".join(covid_article_parts)
+
+
+    with open("text-output.txt", "a+") as file_object:
+        file_object.write(str(covid_article))
---
+++ WriteTest.py
@@ -0,0 +1,2 @@
+with open("text-output.txt", "a+") as file_object:
+    file_object.write("Test")
