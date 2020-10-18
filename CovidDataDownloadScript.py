0a7993ff57161c608ec3885a732a5ffb45775243
justawanderingcloud <jakub.stastka@gmail.com>
2020-10-18 14:12:54 +0200

    new features

--- CovidDataDownloadScript.py
+++ CovidDataDownloadScript.py
@@ -3,4 +3,8 @@ from CovidDataGetter import GetCovidData
 url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json"
 output = "zakladni-prehled.json"
 
+url2 = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/kraj-okres-nakazeni-vyleceni-umrti.csv"
+output2 = "kraj-okres-nakazeni-vyleceni-umrti.csv"
+
 GetCovidData(url, output)
+GetCovidData(url2, output2)
