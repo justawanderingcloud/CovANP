a8806f3d7f2bddb45a2b3e1e4c726ba933f344f8
justawanderingcloud <jakub.stastka@gmail.com>
2020-10-18 16:45:36 +0200

    fixed few things

--- CovidDataDownloadScript.py
+++ CovidDataDownloadScript.py
@@ -1,4 +1,4 @@
-from CovidDataGetter import GetCovidData
+from CovidDataGetter import get_covid_data_json, get_covid_data_csv
 
 url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json"
 output = "zakladni-prehled.json"
@@ -6,5 +6,5 @@ output = "zakladni-prehled.json"
 url2 = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/kraj-okres-nakazeni-vyleceni-umrti.csv"
 output2 = "kraj-okres-nakazeni-vyleceni-umrti.csv"
 
-GetCovidData(url, output)
-# GetCovidData(url2, output2)
+get_covid_data_json(url, output)
+get_covid_data_csv(url2, output2)
--- CovidDataGetter.py
+++ CovidDataGetter.py
@@ -3,9 +3,13 @@
 # with csv downloads entire page for some reason
 
 import requests
+import urllib.request
 
 
-def GetCovidData(url, output):
+def get_covid_data_json(url, output):
     resp = requests.get(url)
     with open(output, "w") as output:
         output.write(resp.text)
+
+def get_covid_data_csv(url, output):
+    urllib.request.urlretrieve(url, output)
