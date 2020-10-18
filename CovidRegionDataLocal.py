4fb0f6de90802cbbfb996166e07e73855959e136
justawanderingcloud <jakub.stastka@gmail.com>
2020-10-18 23:52:18 +0200

    new stuff

--- CovidRegionDataLocal.py
+++ CovidRegionDataLocal.py
@@ -2,6 +2,7 @@
 # parametrize for input region
 
 import pandas as pd
+import random
 
 # pd.set_option("display.max_rows", None, "display.max_columns", None)
 
@@ -18,12 +19,12 @@ def covid_region_article_build(region_source):
 
     str_cz_processed = str_cz.replace(["CZ020", "CZ0201", "CZ0202", "CZ0203", "CZ0204", "CZ0205", "CZ0206", "CZ0207",
                                        "CZ0208", "CZ0209", "CZ020A", "CZ020B", "CZ020C"],
-                                      ["Stredocesky kraj", "Okres Benešov", "Okres Beroun", "Okres Kladno",
-                                       "Okres Kolín", "Okres Kutná Hora", "Okres Mělník", "Okres Mladá Boleslav",
-                                       "Okres Nymburk", "Okres Praha-východ", "Okres Praha-západ", "Okres Příbram",
-                                       "Okres Rakovník"])
+                                      ["Stredocesky kraj", "Benešov", "Beroun", "Kladno",
+                                       "Kolín", "Kutná Hora", "Mělník", "Mladá Boleslav",
+                                       "Nymburk", "Praha-východ", "Praha-západ", "Příbram",
+                                       "Rakovník"])
 
-    # str_cz_processed.to_csv(r"output.csv", index=False)
+    str_cz_processed.to_csv(r"output.csv", index=False)
 
     str_cz_sorted_total_infected = str_cz_processed["kumulativni_pocet_nakazenych"].sum()
     str_cz_sorted_total_cured = str_cz_processed["kumulativni_pocet_vylecenych"].sum()
@@ -48,16 +49,45 @@ def covid_region_article_build(region_source):
     str_cz_sorted_most_cured_list = str_cz_sorted_most_cured.values.tolist()[0]
     str_cz_sorted_least_cured_list = str_cz_sorted_least_cured.values.tolist()[0]
 
-    template1_1 = "Nejhůře na tom je {} s {} nemocnými."
-    template1_2 = "Nejlépe na tom je {} s {} nemocnými."
-    template2_1 = "V okrese {} se z covidu vylecilo nejvice pacientu z kraje: {} lidi."
-    template2_2 = "Naopak v okrese {} se vylecilo nejmene pacientu, tedy {}."
+    str_cz_sorted_most_dead_list = str_cz_sorted_most_dead.values.tolist()[0]
+    str_cz_sorted_least_dead_list = str_cz_sorted_least_dead.values.tolist()[0]
 
-    print("Celkovy pocet nakazenych v STC kraji je", str_cz_sorted_total_infected)
-    print("Celkovy pocet vylecenych v STC kraji je", str_cz_sorted_total_cured)
-    print("Celkovy pocet zemrelych v STC kraji je", str_cz_sorted_total_dead)
+    template_most_infected = ["Nejhůře na tom je okres {} s {} nemocnými covid-19. ",
+                              "Situace je z kraje nejlepší v okrese {}, kde je v současné době {} nemocných. "]
+    template_least_infected = ["Nejlépe na tom je okres {} s {} nemocnými. ",
+                               "Naopak nejlépe se daří okresu {}, kde je pouze {} nemocných. "]
+    template_most_cured = ["V okrese {} se z nemoci covid-19 vyléčilo nejvíce pacientů z kraje, a sice  {} lidí. ",
+                           "Nejvíce vyléčených zaznameni hygienici v okrese {}, kde je takových pacientů {} ."]
+    template_least_cured = ["Dosud se nejméně pacientů vyléčilo na okrese {}, nemoci se zbavilo pouze {} nemocných. ",
+                            "Okres {} má v tuto chvíli nejméně vyléčených, a sice {} pacientů. "]
+    template_most_dead = ["Na nemoc covid-19 v okrese {} zemřelo nejvíce lidí: {} nemocných. ",
+                          "Nemoci covid-19 podlehlo z kraje nejvíce lidí v okrese {}: a sice {} obětí. "]
+    template_least_dead = ["Nejméně lidí podlehlo koronaviru v okrese {}. Jedná se o {} nemocných, kteří zemřeli. ",
+                           "V okrese {} zemřelo na koronavirus {} lidí, což je nejméně z kraje. "]
 
-    print(template1_1.format(*str_cz_sorted_most_infected_list))
-    print(template1_2.format(*str_cz_sorted_least_infected_list))
-    print(template2_1.format(*str_cz_sorted_most_cured_list))
-    print(template2_2.format(*str_cz_sorted_least_cured_list))
+    template_most_infected_selected = random.sample(template_most_infected, 1)[0]
+    template_least_infected_selected = random.sample(template_least_infected, 1)[0]
+    template_most_cured_selected = random.sample(template_most_cured, 1)[0]
+    template_least_cured_selected = random.sample(template_least_cured, 1)[0]
+    template_most_dead_selected = random.sample(template_most_dead, 1)[0]
+    template_least_dead_selected = random.sample(template_least_dead, 1)[0]
+
+    stc_cz_total_infected_article = "Ve Středočeském kraji se v současné době " + str(str_cz_sorted_total_infected) + " nakažených lidí nemocí covid-19. "
+    stc_cz_total_cured_article = "Dosud se vyléčilo " + str(str_cz_sorted_total_cured) + " pacientů. "
+    stc_cz_total_dead_article = "Celkový počet obětí nemoci covid-19 je " + str(str_cz_sorted_total_dead) + " pacientů. "
+
+    stc_cz_most_infected_article = template_most_infected_selected.format(*str_cz_sorted_most_infected_list)
+    stc_cz_least_infected_article = template_least_infected_selected.format(*str_cz_sorted_least_infected_list)
+    stc_cz_most_cured_article = template_most_cured_selected.format(*str_cz_sorted_most_cured_list)
+    stc_cz_least_cured_article = template_least_cured_selected.format(*str_cz_sorted_least_cured_list)
+    stc_cz_most_dead_article = template_most_dead_selected.format(*str_cz_sorted_most_dead_list)
+    stc_cz_least_dead_article = template_least_dead_selected.format(*str_cz_sorted_least_dead_list)
+
+    stc_covid_regional_article_parts = [stc_cz_total_infected_article, stc_cz_total_cured_article,
+                                        stc_cz_total_dead_article, stc_cz_most_infected_article,
+                                        stc_cz_least_infected_article, stc_cz_most_cured_article,
+                                        stc_cz_least_cured_article, stc_cz_most_dead_article,
+                                        stc_cz_least_dead_article]
+
+    stc_covid_regional_article = "".join(stc_covid_regional_article_parts)
+    return stc_covid_regional_article
--- Infobuild.py
+++ Infobuild.py
@@ -2,4 +2,4 @@ from CovidDataLocal import covid_general_article_build
 from CovidRegionDataLocal import covid_region_article_build
 
 print(covid_general_article_build("zakladni-prehled.json"))
-covid_region_article_build("kraj-okres-nakazeni-vyleceni-umrti.csv")
+print(covid_region_article_build("kraj-okres-nakazeni-vyleceni-umrti.csv"))
