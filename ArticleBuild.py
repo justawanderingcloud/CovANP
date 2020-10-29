4cea6d559f361642d319f04f4a6bf381cea92805
justawanderingcloud <jakub.stastka@gmail.com>
2020-10-29 21:36:02 +0100

    rework

--- ArticleBuild.py
+++ ArticleBuild.py
@@ -1,5 +1,8 @@
 from CovidDataLocal import covid_general_article_build
 from CovidRegionDataLocal import covid_region_article_build
 
-print(covid_general_article_build("zakladni-prehled.json"))
-print(covid_region_article_build("kraj-okres-nakazeni-vyleceni-umrti.csv"))
+general_article = covid_general_article_build("zakladni-prehled.json")
+regional_article = covid_region_article_build("kraj-okres-nakazeni-vyleceni-umrti.csv")
+
+print(general_article)
+print(regional_article)
